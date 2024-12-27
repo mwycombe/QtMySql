8472a64ed93aa9c4302b1be8bc095afb174d3e28
Michael Wycombe <michael.wycombe@gmail.com>
2024-12-26 20:25:02 -0800

    ctrl variable egs

--- UiTraining/CheckboxApp.py
+++ UiTraining/CheckboxApp.py
@@ -1,6 +1,8 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QCheckBox
+
 from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QCheckBox
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -21,6 +23,7 @@ class MainWindow(QMainWindow):
     def show_state(self, state):
         print(state == Qt.CheckState.Checked.value)
         print(state)
+
 
 app = QApplication(sys.argv)
 
